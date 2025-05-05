02f0ebd47dc2858f3fd40a430698d9ef56b96e27
gsunkersett <gaurangs@hotmail.com>
2025-05-04 23:22:30 -0700

    more changes

--- finance_agent.py
+++ finance_agent.py
@@ -7,9 +7,11 @@ agent = Agent(
     model=Gemini(id="gemini-2.0-flash"),
     #model=Gemini(id="gemini-1.5-flash"),
     tools=[YFinanceTools(stock_price=True)],
-    instructions=["Always include sources"],
+    #instructions=["Always include sources"],
     show_tool_calls=True,
     markdown=True,
+    debug_mode=False,
+    stream=True,
 )
 
 agent.print_response("What is the stock price of NVDA and TSLA", stream=True)
--- web_search.py
+++ web_search.py
@@ -3,15 +3,21 @@ from phi.model.google import Gemini
 from phi.tools.duckduckgo import DuckDuckGo
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.wikipedia import WikipediaTools
-# from phi.tools.google_search import GoogleSearch
+from phi.tools.googlesearch import GoogleSearch
+from phi.tools.calculator import Calculator
 
 web_agent = Agent(
     name="Web Agent",
     model=Gemini(id="gemini-2.0-flash"),
-    tools=[WikipediaTools(), DuckDuckGo(), YFinanceTools(stock_price=True)],
-    instructions=["Always use tools. Always include sources"],
+    tools=[GoogleSearch(), WikipediaTools(), DuckDuckGo(), YFinanceTools(stock_price=True), Calculator()],
+    #instructions=["Always use tools. Always include sources"],
     show_tool_calls=True,
     markdown=True,
-)
-web_agent.print_response("Who is the current president of the United States?", stream=True)
-#web_agent.print_response("What time is it?", stream=True)
+    stream=True,
+    verbose=True,
+    debug_mode=False,
+    )   
+#web_agent.print_response("Who is the current president of the United States?")
+#web_agent.print_response("What is the stock price of Microsoft and who is their CEO?")
+#web_agent.print_response("What is the latest news on AI?")
+web_agent.print_response("What is 2^20?")
